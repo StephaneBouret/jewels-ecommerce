fbfafcff659045dcb8a0085077c2a2938c6d9772
Stéphane Bouret <sbouret@orange.fr>
2026-03-31 08:40:41 +0200

    Modification du SecurityController en gardant une forme simple, propagation du _target_path dans le formulaire de login et default_target_path comme filet de sécurité

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,7 +9,6 @@ use App\Service\PasswordResetService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,18 +16,9 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    public function __construct(protected RequestStack $requestStack) {}
-
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $session = $this->requestStack->getSession();
-
-        $targetPath = $request->query->get('target', $session->get('_security.main.target_path'));
-        if ($targetPath) {
-            $session->set('_security.main.target_path', $targetPath);
-        }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
@@ -116,19 +106,5 @@ class SecurityController extends AbstractController
         return $this->render('security/reset_password.html.twig', [
             'passForm' => $form
         ]);
-    }
-
-    #[Route(path: '/login/success', name: 'app_login_success')]
-    public function loginSuccess(): Response
-    {
-        $session = $this->requestStack->getSession();
-
-        // Récupérer l'URL de redirection stockée
-        $targetPath = $session->get('_security.main.target_path', $this->generateUrl('app_home'));
-
-        // Supprimer la valeur de la session pour éviter une redirection non voulue plus tard
-        $session->remove('_security.main.target_path');
-
-        return $this->redirect($targetPath);
     }
 }
